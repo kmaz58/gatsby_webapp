a151df8db9b398b8617c804c535a831100b7282e
kmaz <kmaz58@gmail.com>
2022-03-19 15:29:35 +0200

    Add Disable Companies

--- src/pages/check_elta.js
+++ src/pages/check_elta.js
@@ -1,12 +1,306 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
+import { Button, Card, Col, Row, Form, Table } from 'react-bootstrap';
+import axios from 'axios'
+import MainLayout from '../components/layouts/MainLayout';
+import fileDownload from 'js-file-download'
 
-import MainLayout from "../components/layouts/MainLayout"
-import Img_404 from "../images/404.gif"
 
-const Contact = () => (
-  <MainLayout>
-    <h2>Check Elta</h2>
-  </MainLayout>
-)
 
-export default Contact
+
+
+const ConfigPage = () => {
+  const [disable_company, setDisableCompany] = useState(null);
+  const [enable_company, setEnableCompany] = useState(null);
+  const [tableData, setTableData] = useState([]);
+
+
+  const CodCheck = () => {
+    axios({
+      method: 'post',
+      mode: "no-cors",
+      // url: 'https://kmaz.pythonanywhere.com/cdc',
+      url: 'http://localhost:8001/cdc',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    }).then(res => {
+      setTableData(res.data);
+      console.log(tableData)
+      console.log(res)
+    }).catch(error => {
+      console.error(error)
+    });
+  }
+
+  const enabled_products = () => {
+    axios({
+      method: 'post',
+      mode: "no-cors",
+      url: 'https://kmaz.pythonanywhere.com/esr',
+      //url: 'http://localhost:8001/esr',
+      data: { enable_company },
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    }).then(res => {
+      //console.log(res)
+      let extension = ".csv"
+      let filename = enable_company.concat(extension);
+      fileDownload(res.data, filename)
+    }).catch(error => {
+      console.error(error)
+    });
+  }
+
+  const esr = e => {
+    e.preventDefault()
+
+    //console.log(email);
+    //console.log(Password);
+
+    enabled_products();
+  }
+
+  const cdc = e => {
+    e.preventDefault()
+
+    //console.log(email);
+    //console.log(Password);
+
+    CodCheck();
+  }
+  // useEffect(() => {
+  //    const interval = setInterval(wifiSettingsRequest, 5000)
+
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [])
+
+  const renderTableContent = () => {
+    const data = [
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '111',
+        orderId: 1,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '222',
+        orderId: 2,
+        data: '12/03/2022',
+        status: true,
+      },
+      {
+        voucher: '333',
+        orderId: 3,
+        data: '12/03/2022',
+        status: false,
+      },
+      {
+        voucher: '444',
+        orderId: 4,
+        data: '12/03/2022',
+        status: true,
+      },
+    ]
+
+    return tableData.map(item => (
+      <tr>
+        <td>{item.voucher}</td>
+        <td>{item.orderId}</td>
+        <td>{item.data}</td>
+        <td>{item.status}</td>
+      </tr>
+    ))
+  }
+
+  return (
+    <MainLayout>
+      <Row>
+        <Form onSubmit={cdc}>
+          <Form.Group className="mb-3" controlId="cdc">
+            <Form.Label>CodCheck</Form.Label>
+          </Form.Group>
+          <Row className="mt-5">
+            <Col className="mt-4" md={{ span: 4, offset: 2 }} >
+              <Button variant="primary" type="submit" size="lg" >
+                Λήψη
+              </Button>
+            </Col>
+            <Col>
+              <p className="text-right">Λίγες Οδηγίες. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </Col>
+          </Row>
+          <Row>
+            <div style={{ maxHeight: '500px', overflowY: 'auto' }}>
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Voucher</th>
+                    <th>Order ID</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody >
+                  {renderTableContent()}
+                </tbody>
+              </Table>
+            </div>
+
+          </Row>
+        </Form>
+      </Row>
+    </MainLayout >
+  )
+}
+
+export default ConfigPage
--- src/pages/config.js
+++ src/pages/config.js
@@ -14,12 +14,12 @@ const ConfigPage = () => {
   const [enable_company, setEnableCompany] = useState(null);
 
 
-  const disabled_products = () => {
+  const enabled_products = () => {
     axios({
       method: 'post',
       mode: "no-cors",
-      url: 'https://kmaz.pythonanywhere.com/dsr',
-      data: { disable_company },
+      url: 'https://kmaz.pythonanywhere.com/esr',
+      data: { enable_company },
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
@@ -27,7 +27,7 @@ const ConfigPage = () => {
     }).then(res => {
       console.log(res)
       let extension = ".csv"
-      let filename = disable_company.concat(extension);
+      let filename = enable_company.concat(extension);
       fileDownload(res.data, filename)
 
     }).catch(error => {
@@ -35,13 +35,13 @@ const ConfigPage = () => {
     });
   }
 
-  const enabled_products = () => {
+  const disabled_products = () => {
     axios({
       method: 'post',
       mode: "no-cors",
-      url: 'https://kmaz.pythonanywhere.com/esr',
-      //url: 'http://localhost:8001/esr',
-      data: { enable_company },
+      //url: 'https://kmaz.pythonanywhere.com/dsr',
+      url: 'http://localhost:8001/dsr',
+      data: { disable_company },
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
@@ -49,7 +49,7 @@ const ConfigPage = () => {
     }).then(res => {
       //console.log(res)
       let extension = ".csv"
-      let filename = enable_company.concat(extension);
+      let filename = disable_company;
       fileDownload(res.data, filename)
     }).catch(error => {
       console.error(error)
@@ -86,11 +86,11 @@ const ConfigPage = () => {
       <Row>
         <Col>
           <Card className="p-2">
-            <Form onSubmit={dsr}>
-              <Form.Group className="mb-3" controlId="dsr">
-                <Form.Label>Απενεργοποιήσεις Προϊόντων</Form.Label>
+            <Form onSubmit={esr}>
+              <Form.Group className="mb-3" controlId="esr">
+                <Form.Label>Eνεργοποιήσεις Προϊόντων</Form.Label>
               </Form.Group>
-              <Form.Select aria-label="Select Carrier" onChange={(e) => setDisableCompany(e.target.value)} type="type">
+              <Form.Select aria-label="Select Carrier" onChange={(e) => setEnableCompany(e.target.value)} type="type">
                 <option value="1">Select</option>
                 <option value="Kikkaboo">Kikkaboo</option>
                 <option value="Lorelli">Lorelli</option>
@@ -112,15 +112,19 @@ const ConfigPage = () => {
         </Col>
         <Col>
           <Card className="p-2">
-            <Form onSubmit={esr}>
-              <Form.Group className="mb-3" controlId="esr">
-                <Form.Label>Eνεργοποιήσεις Προϊόντων</Form.Label>
+            <Form onSubmit={dsr}>
+              <Form.Group className="mb-3" controlId="dsr">
+                <Form.Label>Απενεργοποιήσεις Προϊόντων</Form.Label>
               </Form.Group>
-              <Form.Select aria-label="Select Carrier" onChange={(e) => setEnableCompany(e.target.value)} type="type">
-                <option value="Kikkaboo">Kikkaboo</option>
-                <option value="Lorelli">Lorelli</option>
-                <option value="Bebestars">Bebestars</option>
-                <option value="Cangaroo">Cangaroo</option>
+              <Form.Select aria-label="Select Carrier" onChange={(e) => setDisableCompany(e.target.value)} type="type">
+                <option value="Kikka Boo.csv">Kikka Boo</option>
+                <option value="Lorelli.csv">Lorelli</option>
+                <option value="Bebestars.csv">Bebestars</option>
+                <option value="Cangaroo.csv">Cangaroo</option>
+                <option value="Dimcol.xlsx">Dimcol</option>
+                <option value="Le Blanc.xlsx">Le Blanc</option>
+                <option value="Beauty Home.xlsx">Beauty Home</option>
+                <option value="Baby Oliver.xlsx">Baby Oliver</option>
               </Form.Select>
               <Row className="mt-5">
                 <Col className="mt-4" md={{ span: 4, offset: 2 }} >
