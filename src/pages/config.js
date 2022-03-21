676550e110cae59838e3a039173f7616f0abb158
kmaz <kmaz58@gmail.com>
2022-03-21 17:28:38 +0200

    response Type changed to blob

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
-import { Button, Card, Col, Row, Form } from 'react-bootstrap';
+import { Button, Card, Col, Row, Form, Spinner } from 'react-bootstrap';
 import axios from 'axios'
 import MainLayout from '../components/layouts/MainLayout';
 import fileDownload from 'js-file-download'
@@ -12,6 +12,8 @@ import fileDownload from 'js-file-download'
 const ConfigPage = () => {
   const [disable_company, setDisableCompany] = useState(null);
   const [enable_company, setEnableCompany] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
 
 
   const enabled_products = () => {
@@ -36,11 +38,13 @@ const ConfigPage = () => {
   }
 
   const disabled_products = () => {
+    setIsLoading(true);
     axios({
       method: 'post',
+      responseType: 'blob',
       mode: "no-cors",
-      url: 'https://kmaz.pythonanywhere.com/dsr',
-      //url: 'http://localhost:8001/dsr',
+      //url: 'https://kmaz.pythonanywhere.com/dsr',
+      url: 'http://localhost:8001/dsr',
       data: { disable_company },
       headers: {
         'Content-Type': 'application/json',
@@ -48,7 +52,7 @@ const ConfigPage = () => {
       }
     }).then(res => {
       //console.log(res)
-      let extension = ".csv"
+      setIsLoading(false)
       let filename = disable_company;
       fileDownload(res.data, filename)
     }).catch(error => {
@@ -58,7 +62,6 @@ const ConfigPage = () => {
 
   const esr = e => {
     e.preventDefault()
-
     //console.log(email);
     //console.log(Password);
 
@@ -73,6 +76,8 @@ const ConfigPage = () => {
 
     disabled_products();
   }
+
+
   // useEffect(() => {
   //    const interval = setInterval(wifiSettingsRequest, 5000)
 
@@ -80,6 +85,8 @@ const ConfigPage = () => {
   //     clearInterval(interval)
   //   }
   // }, [])
+
+
 
   return (
     <MainLayout>
@@ -116,7 +123,8 @@ const ConfigPage = () => {
               <Form.Group className="mb-3" controlId="dsr">
                 <Form.Label>Απενεργοποιήσεις Προϊόντων</Form.Label>
               </Form.Group>
-              <Form.Select aria-label="Select Carrier" onChange={(e) => setDisableCompany(e.target.value)} type="type">
+              <Form.Select aria-label="Select Supplier" onChange={(e) => setDisableCompany(e.target.value)} type="type">
+                <option value="1">Select</option>
                 <option value="Kikka Boo.csv">Kikka Boo</option>
                 <option value="Lorelli.csv">Lorelli</option>
                 <option value="Bebestars.csv">Bebestars</option>
@@ -129,6 +137,15 @@ const ConfigPage = () => {
               <Row className="mt-5">
                 <Col className="mt-4" md={{ span: 4, offset: 2 }} >
                   <Button variant="primary" type="submit" size="lg" >
+                    {isLoading &&
+                      <Spinner
+                        className="mr-5"
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />}
                     Λήψη
                   </Button>
                 </Col>
